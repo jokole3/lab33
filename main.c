2ca489f2cf90b726e4f31751cca545b7f9038e39
jokole3 <paesdoorball@mail.ru>
2024-11-15 15:47:30 +0500

    Вот это изменить.
    Signed-off-by: jokole3 <paesdoorball@mail.ru>

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include "askname.h" 
 int main(int argc, char **argv) 
 { 
-    char first[255], last[255];
     char first[255], last[255]; 
+    char first[255]; 
     askname(first); 
+    printf("Hello, %s %s!\n", first, last); 
     printf("Hello, %s %s!\n", first, last); 
     return 0; 
 } 
