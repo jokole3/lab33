b859af8fd9f7563469720f9c42880266c05cccd3
jokole3 <paesdoorball@mail.ru>
2024-11-15 15:39:02 +0500

    Удалить 5 строку!
    Signed-off-by: jokole3 <paesdoorball@mail.ru>
    
    Готово!

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main(int argc, char **argv) 
 { 
     char first[255], last[255];
-    char first[255], last[255]; 
     askname(first); 
-    printf("Hello, %s %s!\n", first, last); 
+    printf("Hey, %s %s!\n", first, last); 
+    printf("Hi, %s %s!\n", first, last); 
     return 0; 
 } 
