31a13a6603c082a48ac6973a8bdcc2c2de977f27
jokole3 <paesdoorball@mail.ru>
2024-11-15 17:11:40 +0500

    Подходит!

--- main.c
+++ main.c
@@ -5,6 +5,5 @@ int main(int argc, char **argv)
     char first[255]; 
     askname(first); 
     printf("Hello, %s %s!\n", first, last); 
-    printf("Hello, %s %s!\n", first, last); 
     return 0; 
 } 
